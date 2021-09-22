40a90f407d1b4873bcde7e9c8d23635516b39ab8
rajeev <rajeev33parmar@gmail.com>
2021-09-22 14:22:00 +0530

    friends app

---
+++ config/routes.rb
@@ -0,0 +1,20 @@
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+Rails.application.routes.draw do
+  devise_for :users
+  resources :friends
+  # get 'home/index'
+
+
+
+   root  'friends#index'
+
+
+
+  get   'home/about'    
+
+
+
+
+end
